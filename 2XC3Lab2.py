823fc304a9de3e46b51b9b0d8aa9fdc79ecea4d0
Paarth Kadakia <73555792+barmeloxanthony@users.noreply.github.com>
2023-01-25 09:50:28 -0500

    Add files via upload

---
+++ 2XC3Lab2.py
@@ -0,0 +1,181 @@
+"""
+This file corresponds to the first graded lab of 2XC3.
+Feel free to modify and/or add functions to this file.
+"""
+import random
+import random
+import timeit
+import matplotlib.pyplot as plot
+
+
+# Create a random list length "length" containing whole numbers between 0 and max_value inclusive
+def create_random_list(length, max_value):
+    return [random.randint(0, max_value) for _ in range(length)]
+
+
+# Creates a near sorted list by creating a random list, sorting it, then doing a random number of swaps
+def create_near_sorted_list(length, max_value, swaps):
+    L = create_random_list(length, max_value)
+    L.sort()
+    for _ in range(swaps):
+        r1 = random.randint(0, length - 1)
+        r2 = random.randint(0, length - 1)
+        swap(L, r1, r2)
+    return L
+
+
+# I have created this function to make the sorting algorithm code read easier
+def swap(L, i, j):
+    L[i], L[j] = L[j], L[i]
+
+
+# ******************* Insertion sort code *******************
+
+# This is the traditional implementation of Insertion Sort.
+def insertion_sort(L):
+    for i in range(1, len(L)):
+        insert(L, i)
+
+
+def insert(L, i):
+    while i > 0:
+        if L[i] < L[i-1]:
+            swap(L, i-1, i)
+            i -= 1
+        else:
+            return
+
+
+# This is the optimization/improvement we saw in lecture
+def insertion_sort2(L):
+    for i in range(1, len(L)):
+        insert2(L, i)
+
+
+def insert2(L, i):
+    value = L[i]
+    while i > 0:
+        if L[i - 1] > value:
+            L[i] = L[i - 1]
+            i -= 1
+        else:
+            L[i] = value
+            return
+    L[0] = value
+
+
+# ******************* Bubble sort code *******************
+
+# Traditional Bubble sort
+def bubble_sort(L):
+    for i in range(len(L)):
+        for j in range(len(L) - 1):
+            if L[j] > L[j+1]:
+                swap(L, j, j+1)
+
+
+# ******************* Selection sort code *******************
+
+# Traditional Selection sort
+def selection_sort(L):
+    for i in range(len(L)):
+        min_index = find_min_index(L, i)
+        swap(L, i, min_index)
+
+
+def find_min_index(L, n):
+    min_index = n
+    for i in range(n+1, len(L)):
+        if L[i] < L[min_index]:
+            min_index = i
+    return min_index
+
+total1 = 0
+total2 = 0
+total3 = 0
+total4 = 0
+data = []
+n = 1000
+k = 100
+
+for _ in range(n):
+    L = create_random_list(k, k)
+    L2 = L.copy()
+    L3 = L.copy()
+    L4 = L.copy()
+
+    start = timeit.default_timer()
+    insertion_sort(L)
+    end = timeit.default_timer()
+    total1 += end - start
+
+    start = timeit.default_timer()
+    insertion_sort2(L2)
+    end = timeit.default_timer()
+    total2 += end - start
+
+    start = timeit.default_timer()
+    bubble_sort(L3)
+    end = timeit.default_timer()
+    total3 += end - start
+
+    start = timeit.default_timer()
+    selection_sort(L4)
+    end = timeit.default_timer()
+    total4 += end - start
+
+
+# step is space between
+# n is highest length list
+# m is number of lists
+def experiement1(n, m, step):
+    times = []
+    for i in range(0, n, step):
+        L = create_random_list(i, i)
+        time = 0
+        for _ in range(m):
+            start = timeit.default_timer()
+            insertion_sort(L)
+            end = timeit.default_timer()
+            time += end - start
+            times.append(time)
+    return times
+
+def experiement2(n, m, step):
+    times = []
+    for i in range(0, n, step):
+        L = create_random_list(i, i)
+        time = 0
+        for _ in range(m):
+            start = timeit.default_timer()
+            selection_sort(L)
+            end = timeit.default_timer()
+            time += end - start
+            times.append(time)
+    return times
+
+def experiement3(n, m, step):
+    times = []
+    for i in range(0, n, step):
+        L = create_random_list(i, i)
+        time = 0
+        for _ in range(m):
+            start = timeit.default_timer()
+            bubble_sort(L)
+            end = timeit.default_timer()
+            time += end - start
+            times.append(time)
+    return times
+
+
+times = experiement1(1000, 10, 1000)
+plot.plot(times)
+plot.show()
+
+times = experiement1(1000, 100, 1000)
+plot.plot(times)
+plot.show()
+
+
+
+# print("Improvement of ", (1 - total2/total1) * 100, "%")
